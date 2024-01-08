3ce4a5b40965ac2cf3b36c5af9428914871d47e3
TeamYummi <156001259+TeamYummi@users.noreply.github.com>
2024-01-08 22:59:55 +0530

    YummiMusicBot

--- YummiMusic/plugins/tools/ping.py
+++ YummiMusic/plugins/tools/ping.py
@@ -1,11 +1,4 @@
-#
-# Copyright (C) 2021-2022 by TeamYummi@Github, < https://github.com/TeamYummi >.
-#
-# This file is part of < https://github.com/TeamYummi/YummiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYummi/YummiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
+# 𝗗𝗲𝘃𝗹𝗼𝗽𝗲𝗱 𝗕𝘆 𝗧𝗲𝗮𝗺 𝗬𝘂𝗺𝗺𝗶
 
 from datetime import datetime
 
